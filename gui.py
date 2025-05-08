84ed7cbef655d44e1895d1621c4c90f71a74e23b
Aidan Wallis <45044731+aidanwallis@users.noreply.github.com>
2025-05-07 19:26:15 -0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,20 +1,47 @@
 import flet as ft
-import asyncio
 from stopwatch import Stopwatch
 
 def main(page: ft.Page):
 
     page.title = "Stopwatch"
-    stopwatch = Stopwatch(40)
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    button_state = True
+    
+    def button_click(e):
+        nonlocal button_state
+        button_state = not button_state
 
-    def toggle_off(e):
+        if button_state:
+            start_stop_button.text = "Start"
+            toggle_off()
+        else:
+            start_stop_button.text = "Stop"
+            toggle_on()
+        page.update()
+
+    def toggle_off():
         stopwatch.update_toggle(False, page)
-        page.update(e)
+        page.update()
 
-    def toggle_on(e):
+    def toggle_on():
         stopwatch.update_toggle(True, page)
-        page.update(e)
+        page.update()
+
+    def reset_click(e):
+        stopwatch.reset()
+        page.update()
+
+    stopwatch = Stopwatch(60)
+    start_stop_button = ft.ElevatedButton(text="Start", on_click=button_click, width=150, height=60)
+    reset_button = ft.ElevatedButton(text='Reset', on_click=reset_click, width=150, height=60)
+    buttons = ft.Row(
+                [
+                start_stop_button,
+                reset_button
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            )
 
     page.add(
         ft.Row(
@@ -22,8 +49,7 @@ def main(page: ft.Page):
             ft.Column(
                 [
                     stopwatch,
-                    ft.ElevatedButton(text = 'Start', on_click = toggle_on, width = 150, height = 60),
-                    ft.ElevatedButton(text = 'Stop', on_click = toggle_off, width = 150, height = 60)
+                    buttons
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
